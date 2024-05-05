435157a197f6ae882741a39d21d8f15599e449d3
Abraxas-365 <luisfmiranda8@gmail.com>
2024-05-05 12:57:47 -0500

    feat(qr-module): add get qr function in api handler

--- src/modules/qr/api/route_config.rs
+++ src/modules/qr/api/route_config.rs
@@ -1,6 +1,6 @@
 use actix_web::web;
 
-use super::handler::{create_qr, delete_owner, set_owner};
+use super::handler::{create_qr, delete_owner, get_qr, set_owner};
 
 pub fn route_config(cfg: &mut web::ServiceConfig) {
     cfg.service(web::resource("/create-qr").route(web::post().to(create_qr)));
@@ -8,4 +8,5 @@ pub fn route_config(cfg: &mut web::ServiceConfig) {
     cfg.service(web::resource("/set-owner").route(web::post().to(set_owner)));
 
     cfg.service(web::resource("/delete-owner").route(web::delete().to(delete_owner)));
+    cfg.service(web::resource("/qr/{id}").route(web::get().to(get_qr)));
 }
