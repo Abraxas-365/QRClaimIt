06546a3935dd907acb24c42ef8b6bc90c808ac45
Abraxas-365 <luisfmiranda8@gmail.com>
2024-05-14 15:23:13 -0500

    migration

--- migrations/0001_init_db.up.sql
+++ migrations/0001_init_db.up.sql
@@ -4,6 +4,5 @@ CREATE TABLE qr (
     password VARCHAR(10) NOT NULL,                 -- Password with a fixed length of 10 characters
     creation_date TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     redemption_date TIMESTAMP WITH TIME ZONE NULL, -- Date of redemption, optional
-    PRIMARY KEY (id),
-    UNIQUE (email)
+    PRIMARY KEY (id)
 );
